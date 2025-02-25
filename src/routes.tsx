54b5070af5cef24194adaee23e97964e7071aa1b
Hyesu <skxldzm123@naver.com>
2025-02-25 23:17:14 +0900

    feat: 파일명 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import SubContainer from "./components/layout/SubContainer";
-import GlobeComponent from "./components/layout/GlobeComponent";
+import GlobeComponent from "./components/layout/globeComponent";
 
 const CountryInfoPage = lazy(
   () => import("./pages/CountryInfo/CountryInfoPage")
